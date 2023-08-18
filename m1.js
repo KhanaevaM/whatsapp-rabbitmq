e297e1c4f56cedd04e28704c0ace89d3a3c7a42d
KhanaevaM <khanaevamariya@gmail.com>
2023-08-18 09:43:24 +0300

    response sendind is added

--- m1.js
+++ m1.js
@@ -3,7 +3,8 @@ const amqp = require('amqplib');
 const IsRunningInContainer = require('./containerUtils');
 
 const app = express();
-const QUEUE_NAME = 'tasks_queue';
+const TASKS_QUEUE_NAME = 'tasks_queue';
+const RESPONSES_QUEUE_NAME = 'responses_queue';
 
 app.use(express.json());
 
@@ -15,8 +16,8 @@ async function sendTaskToQueue(task) {
     const connection = await amqp.connect(`amqp://${host}`);
     const channel = await connection.createChannel();
 
-    await channel.assertQueue(QUEUE_NAME, { durable: true });
-    await channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(task)));
+    await channel.assertQueue(TASKS_QUEUE_NAME, { durable: true });
+    await channel.sendToQueue(TASKS_QUEUE_NAME, Buffer.from(JSON.stringify(task)));
 
     console.log(`Task sent to queue: ${JSON.stringify(task)}`);
     setTimeout(() => connection.close(), 500);
@@ -36,3 +37,25 @@ app.post('/process', (req, res) => {
 app.listen(`3000`, () => {
   console.log(`Microservice M1 is running on http://${host}:3000`);
 });
+
+async function startWorker() {
+  try {
+    const connection = await amqp.connect(`amqp://guest:guest@${host}:5672`);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(RESPONSES_QUEUE_NAME, { durable: true });
+    console.log('Worker is waiting for responses...');
+
+    channel.consume(RESPONSES_QUEUE_NAME, (message) => {
+      const response = JSON.parse(message.content.toString());
+
+      console.log(`Response got: ${JSON.stringify(response)}`);
+
+      channel.ack(message);
+    });
+  } catch (error) {
+    console.error('Error starting worker:', error);
+  }
+}
+
+startWorker();
--- m2.js
+++ m2.js
@@ -1,14 +1,35 @@
 const amqp = require('amqplib');
 const IsRunningInContainer = require('./containerUtils');
 
-const QUEUE_NAME = 'tasks_queue';
+const TASKS_QUEUE_NAME = 'tasks_queue';
+const RESPONSES_QUEUE_NAME = 'responses_queue';
 var host = IsRunningInContainer() ? "rabbitmq" : "localhost";
+
+// connect to RabbitMQ and send respons to queue
+async function sendResponseToQueue(response) {
+  try {
+    const connection = await amqp.connect(`amqp://${host}`);
+    const channel = await connection.createChannel();
+
+    await channel.assertQueue(RESPONSES_QUEUE_NAME, { durable: true });
+    await channel.sendToQueue(RESPONSES_QUEUE_NAME, Buffer.from(JSON.stringify(response)));
+
+    console.log(`Response sent to queue: ${JSON.stringify(response)}`);
+    setTimeout(() => connection.close(), 500);
+  } catch (error) {
+    console.error('Error sending response to queue:', error);
+  }
+}
 
 async function processTask(task) {
   try {
-    // Here we can put our processing code
-
     console.log(`Processing task: ${JSON.stringify(task)}`);
+
+    // Here we can put our processing code
+    const response = {"data": `'${task.data}' is proccessed.`};
+
+    sendResponseToQueue(response);
+
   } catch (error) {
     console.error('Error processing task:', error);
   }
@@ -19,10 +40,10 @@ async function startWorker() {
     const connection = await amqp.connect(`amqp://guest:guest@${host}:5672`);
     const channel = await connection.createChannel();
 
-    await channel.assertQueue(QUEUE_NAME, { durable: true });
+    await channel.assertQueue(TASKS_QUEUE_NAME, { durable: true });
     console.log('Worker is waiting for tasks...');
 
-    channel.consume(QUEUE_NAME, (message) => {
+    channel.consume(TASKS_QUEUE_NAME, (message) => {
       const task = JSON.parse(message.content.toString());
 
       processTask(task);
